7cb888a19ed59196d65be60eea6e4d658f22c63c
chummy-ch <chummy.tema@gmail.com>
2020-04-13 13:44:25 +0300

    weapone struct

---
+++ Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Fiona
+{
+  class Inventory
+  {
+    public string weapon;
+    public int demage;
+    public bool equip;
+
+    Inventory()
+    {
+
+    }
+    
+
+  }
+}
--- Person.cs
+++ Person.cs
@@ -9,23 +9,22 @@ using System.IO;
 
 namespace botfiona
 {
-  [Serializable]
   class Person
   {
     public int Wins { get; set; }
     public string Name { get; set; }
     public int Mes { get; set; }
-/*    public string Weapon { get; set; }
-*/
+    public string Weapon { get; set; }
+
     public List<Person> people = new List<Person>();
 
-    public Person(string name, int mes, int wins/*, string weapon*/)
+    public Person(string name, int mes, int wins, string weapon)
     {
       this.Name = name;
       this.Mes = mes;
       this.Wins = wins;
-/*      this.Weapon = weapon;
-*/      
+      this.Weapon = weapon;
+
     }
 
     public Person()
@@ -33,28 +32,6 @@ namespace botfiona
 
     }
 
-    public  void MakePerson(string UName, int mesC, int winsc/*, string weapons*/)
-    {
-      LoadPeople();
-      Person person = new Person(UName, mesC, winsc);
-
-    }
-
-    public void SavePeople()
-    {
-      using (StreamWriter writer = File.CreateText("persons.txt"))
-      {
-        var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
-        JsonSerializer.Create(settings).Serialize(writer, people);
-      }
-    }
-
-    public void LoadPeople()
-    {
-      if (!File.Exists("persons.txt")) return;
-      string json = File.ReadAllText("persons.txt");
-      people = new JavaScriptSerializer().Deserialize<List<Person>>(json);
-    }
 
   }
 }
