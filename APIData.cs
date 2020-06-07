68f48dcfb2e0a49d8092233378cb1c8364ec14fa
chummy-ch <chummy.tema@gmail.com>
2020-06-07 15:41:31 +0300

    API key added to public view.
    Програму нельзя біло запустить, потому что забыл убрать API key из гитигнора.

---
+++ APIData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Fiona
+{
+  class APIData
+  {
+    public const string key = "905671296:AAExzN80dNrlGv3KE_R6_6ta-rsi7Fpi7Y0";
+  }
+}
